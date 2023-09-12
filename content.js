95b9d4e144931464e296345f6acd9652ac02739c
OsamaNagi <osamanagiworks@gmail.com>
2023-09-13 00:28:11 +0300

    fixing priority loading and some refactoring

--- content.js
+++ content.js
@@ -1,45 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-	chrome.runtime.onMessage.addListener(function (message) {
-		const checker = document.getElementById('checker');
-		checker.textContent = message.message;
-	});
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		// Send a message to the content script to trigger the color change
-		chrome.scripting.executeScript({
-			target: { tabId: tabs[0].id },
-			function: () => {
-				// Define the changeEmailColors function in the content script
-				function changeEmailColors() {
-					const emails = document.querySelector('.aiL'); // Adjust the selector to match Gmail's email elements
-					const content = emails.textContent.toLowerCase();
-					const background = document.querySelector('.gs');
-					rejectionKeywords.forEach((keyword) => {
-						if (content.includes(keyword.toLowerCase())) {
-							chrome.runtime.sendMessage({ message: 'huh, you thought you were good! huh' });
-							background.style.backgroundColor = '#FDA4AF';
-							background.style.border = '2px solid #E11D48';
-							background.style.borderRadius = '5px';
-							background.style.padding = '10px';
-						}
-					});
-
-					acceptanceKeywords.forEach((keyword) => {
-						if (content.includes(keyword.toLowerCase())) {
-							chrome.runtime.sendMessage({ message: "that's why you are the GOAT!!" });
-							background.style.backgroundColor = '#5EEAD4';
-							background.style.border = '2px solid #14B8A6';
-							background.style.borderRadius = '5px';
-							background.style.padding = '10px';
-						}
-					});
-				}
-				// Call the changeEmailColors function
-				changeEmailColors();
-			},
-		});
-	});
-});
-
 const rejectionKeywords = [
 	'another applicant',
 	'Regret',
@@ -52,35 +10,35 @@ const rejectionKeywords = [
 	'We are unable to',
 	'We must decline',
 	"We won't be able to",
-	'We appreciate your effort, but...',
-	'While we value your application, ...',
+	'We appreciate your effort, but',
+	'While we value your application, ',
 	'Your request has not been approved.',
 	'Your proposal does not meet our requirements.',
-	'We cannot move forward with...',
+	'We cannot move forward with',
 	'We have decided to go in a different direction.',
-	'After careful consideration, we have decided...',
+	'After careful consideration, we have decided',
 	'Unfortunately, your request has been denied.',
-	'Although we would have liked to...',
-	'We understand your situation, but...',
-	'We regretfully must decline...',
-	'We wish we could, but...',
-	'Thank you for considering us, but...',
-	'We appreciate your interest, however...',
-	'While your proposal is interesting, we have decided...',
-	'We found some issues with...',
-	'We have concerns about...',
-	'Unfortunately, there are some problems with...',
-	'We are not satisfied with...',
-	'Instead, we suggest...',
-	'You might consider...',
-	'An alternative approach could be...',
-	'Have you thought about...',
-	'It may be challenging to...',
-	'We are unsure if...',
-	'We are hesitant to...',
-	'We are cautious about...',
+	'Although we would have liked to',
+	'We understand your situation, but',
+	'We regretfully must decline',
+	'We wish we could, but',
+	'Thank you for considering us, but',
+	'We appreciate your interest, however',
+	'While your proposal is interesting, we have decided',
+	'We found some issues with',
+	'We have concerns about',
+	'Unfortunately, there are some problems with',
+	'We are not satisfied with',
+	'Instead, we suggest',
+	'You might consider',
+	'An alternative approach could be',
+	'Have you thought about',
+	'It may be challenging to',
+	'We are unsure if',
+	'We are hesitant to',
+	'We are cautious about',
 	'If certain conditions were met, we might reconsider.',
-	'We would be open to discussing this further if...',
+	'We would be open to discussing this further if',
 	'not to proceed',
 	'decided to move ahead with another candidate',
 	'decided to move ahead with another',
@@ -114,3 +72,43 @@ const acceptanceKeywords = [
 	'we are glad to inform you',
 	'you have been shortlisted',
 ];
+
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.runtime.onMessage.addListener(function (message) {
+		const checker = document.getElementById('checker');
+		checker.textContent = message.message;
+	});
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// Send a message to the content script to trigger the color change
+		chrome.scripting.executeScript({
+			target: { tabId: tabs[0].id },
+			function: () => {
+				// Define the changeEmailColors function in the content script
+				function changeEmailColors() {
+					const emails = document.querySelector('.aiL'); // Adjust the selector to match Gmail's email elements
+					const content = emails.textContent.toLowerCase();
+					const background = document.querySelector('.gs');
+
+					const containsAcceptanceKeywords = acceptanceKeywords.some((keyword) => content.includes(keyword.toLowerCase()));
+					const containsRejectionKeywords = rejectionKeywords.some((keyword) => content.includes(keyword.toLowerCase()));
+
+					if (containsAcceptanceKeywords) {
+						chrome.runtime.sendMessage({ message: "that's why you are the GOAT!!" });
+						background.style.backgroundColor = '#5EEAD4';
+						background.style.border = '2px solid #14B8A6';
+						background.style.borderRadius = '5px';
+						background.style.padding = '10px';
+					} else if (containsRejectionKeywords) {
+						chrome.runtime.sendMessage({ message: 'nah!, you thought you were good! huh' });
+						background.style.backgroundColor = '#FDA4AF';
+						background.style.border = '2px solid #E11D48';
+						background.style.borderRadius = '5px';
+						background.style.padding = '10px';
+					}
+				}
+				// Call the changeEmailColors function
+				changeEmailColors();
+			},
+		});
+	});
+});
